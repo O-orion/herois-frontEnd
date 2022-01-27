c71248b907ea617e36cc55c4229f1c116d1289fc
Luca Viana <78035167+O-orion@users.noreply.github.com>
2022-01-26 22:45:50 -0400

    JESUS CRISTO É O SENHOR - HeroComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
